4e8f7d773e3d1a3d1ad61e835e85f9eed43d6210
Suryansh Pandey <coderhood0011@gmail.com>
2024-10-11 10:42:34 +0530

    ApiError, ApiResponse, asyncHandler - configured

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 //require('dotenv').config({path : '../.env'})
 import dotenv from 'dotenv';
 import mongoose from "mongoose";
-import { DB_NAME } from "./constants.js";
 import connectDB from "./db/index.js";
+import express from 'express';
+
+const app = express();
+
 dotenv.config({path : './.env'})
 
 connectDB()
+.then(() => {
+    const port = process.env.PORT || 8000;
+    app.listen(port, () => {
+        console.log(`Server is listening on http://localhost:${port}`);
+    })
+})
+.catch((err) => {
+    console.log("MongoDB connection failed : error from index.js, error : ", err);
+})
